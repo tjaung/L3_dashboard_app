01a27e0800a3fa2bb240f9c62a2cad395ce8fed7
tjaung <jaungt@gmail.com>
2023-01-20 21:46:51 -0500

    app.py run app change

--- app.py
+++ app.py
@@ -1,24 +1,19 @@
 from backend.data_pull import MODELS_BY_PAL
 import backend.generate_dashes as generate_dashes
 
-import explainerdashboard
 from explainerdashboard import ClassifierExplainer, ExplainerDashboard, ExplainerHub
 import dash
-from dash import Dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
 from pathlib import Path
 import os.path
 import importlib
-from flask import render_template
+
 
 app = Dash(__name__, use_pages = False)
 
 hub = generate_dashes.generate_pages()
 hub.to_yaml("hub.yaml", integrate_dashboard_yamls=True)
 
-hub.run()
-
-""" 
 if __name__ == '__main__':
-    app.run_server(debug=True) """
+    app.run_server(debug=True)
